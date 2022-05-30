d9078b6bcbd707d193680705d716418b4d28cf17
mtrblee <changhoblee@outlook.com>
2022-05-30 13:08:44 -0400

    Add initial web project

---
+++ src/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BrianWebsite.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
