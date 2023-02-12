1bdba64ba295bc33dd35c3c7cccc71ee0fa8c443
Brian Lee <changhoblee@outlook.com>
2023-02-12 17:28:35 -0500

    update header bar

---
+++ src/Controllers/ContactController.cs
@@ -0,0 +1,27 @@
+﻿namespace BrianWebsite.Controllers
+{
+    using BrianWebsite.Models;
+    using Microsoft.AspNetCore.Mvc;
+    using System.Diagnostics;
+
+    public class ContactController : Controller
+    {
+        private readonly ILogger<ContactController> _logger;
+
+        public ContactController(ILogger<ContactController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
