a2d5ee8b8be19b494a486a317aeaf75f9ca740f6
Brian Lee <changhoblee@outlook.com>
2024-03-03 17:43:44 -0500

    restructure files

--- src/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace BrianWebsite.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
